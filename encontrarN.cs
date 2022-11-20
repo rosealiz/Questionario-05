36485ac4104eaec393eedc34d97ef5cc5f9adc59
rosealiz <112977051+rosealiz@users.noreply.github.com>
2022-11-20 15:53:26 -0300

    Create encontrarN.cs

---
+++ encontrarN.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class vetorestudo
+{
+    public static void Main()
+    {
+    int[] numero = new int[3];
+    int i, N, posicao=0;
+    Console.WriteLine("digite numeros consecutivamente");
+    for(i=0; i<3; i++){
+        numero[i] = int.Parse(Console.ReadLine());
+    }
+    Console.WriteLine("digite o numero desejado");
+    N = int.Parse(Console.ReadLine());
+    for(i=0; i<3; i++){
+        if(N==numero[i]){
+        posicao = i;
+        Console.WriteLine($"o numero foi encontrado no indice {posicao}");
+        break;
+        }
+     }
+     if(posicao != i){
+        Console.WriteLine("N nao esta presente no vetor");
+        }
+  }
+}
