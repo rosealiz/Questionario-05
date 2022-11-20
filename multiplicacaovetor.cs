eb9afa11e7d74a564bfc93fff4547de8e0d232bb
rosealiz <112977051+rosealiz@users.noreply.github.com>
2022-11-20 13:57:45 -0300

    Create multiplicacaovetor.cs

---
+++ multiplicacaovetor.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class multiplicacaovetor
+{
+    public static void Main()
+    {
+    int[] numero = new int[8];
+    int i=0;
+    double multiplicacao=1;
+    Console.WriteLine("digite numeros consecutivamente");
+    for(i=0; i<8; i++){
+        numero[i] = int.Parse(Console.ReadLine());
+    }
+     Console.WriteLine("esses foram os numeros digitados");
+    for(i=0; i<8; i++){
+    multiplicacao = multiplicacao * numero[i];
+    Console.WriteLine(multiplicacao);
+    }
+    }
+}
